0a738efd3ecd4185790e7b5965eeac9195b134a8
Maycie Morris <maycie.morris@gmail.com>
2020-10-21 13:47:59 -0400

    Deleted CORS

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const helmet = require('helmet');
 const authRouter = require('../auth/auth-router');
 const usersRouter = require('../users/users-router');
@@ -9,7 +9,7 @@ const trucksRouter = require('../trucks/trucks-router');
 const server = express();
 
 server.use(helmet());
-server.use(cors());
+// server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
